5f95a19842a54d0ef1858725cfd6402d1898acac
skarltjr <kisa0828@naver.com>
2021-02-11 15:11:05 +0900

    feat:paging적용

--- src/main/java/class101/foo/io/PostController.java
+++ src/main/java/class101/foo/io/PostController.java
@@ -1,6 +1,10 @@
 package class101.foo.io;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,14 +21,11 @@ public class PostController {
         return postRepository.save(post);
     }
 
-    // 2-1. 글 목록을 조회한다.
+    // 2 글 목록을 페이징하여 반환
     @GetMapping("/posts")
-    public List<Post> getPostList() {
-        return postRepository.findAll();
+    public Page<Post> getPostList(@PageableDefault(size = 20, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
+        return postRepository.findAll(pageable);
     }
-    
-    // 2-2 글 목록을 페이징하여 반환
-    
     // 3. 글 번호로 조회
     
     // 4. 글 내용으로 검색 -> 해당 내용이 포함된 모든 글
